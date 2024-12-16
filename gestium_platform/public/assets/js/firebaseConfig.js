59e68cb98a271747454942242a90ce9c84e8469f
buurguees <alex.burgues16@gmail.com>
2024-12-16 12:17:26 +0000

    loginregister.js actualizado

--- gestium_platform/public/assets/js/firebaseConfig.js
+++ gestium_platform/public/assets/js/firebaseConfig.js
@@ -1,16 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA...",
+    apiKey: "AIzaSyAakN24Y8JP68Z4SD7Vg3hc3p-sxDoUGo4",
     authDomain: "gestium-f7ed4.firebaseapp.com",
     projectId: "gestium-f7ed4",
-    storageBucket: "gestium-f7ed4.appspot.com",
+    storageBucket: "gestium-f7ed4.firebasestorage.app",
     messagingSenderId: "256571486964",
-    appId: "1:256571486964:web:cce69e2665f8e9cb813e4a",
+    appId: "1:256571486964:web:ce69e2665f8e9cb813e4a1"
 };
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
--- gestium_platform/public/assets/js/loginregister.js
+++ gestium_platform/public/assets/js/loginregister.js
@@ -1,29 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import './app.js';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAakN24Y8JP68Z4SD7Vg3hc3p-sxDoUGo4",
-  authDomain: "gestium-f7ed4.firebaseapp.com",
-  projectId: "gestium-f7ed4",
-  storageBucket: "gestium-f7ed4.firebasestorage.app",
-  messagingSenderId: "256571486964",
-  appId: "1:256571486964:web:ce69e2665f8e9cb813e4a1"
-};
+// Selección del formulario usando el ID correcto
+const signupForm = document.querySelector('#register');
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Evento submit para el formulario
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault(); // Evita que la página se recargue
 
+  // Captura de valores de los campos de entrada
+  const name = document.querySelector('#username').value;
+  const lastname = document.querySelector('#userlastname').value;
+  const email = document.querySelector('#useremail').value;
+  const password = document.querySelector('#userpassword').value;
 
-//inputs
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
-
-//submit button
-const submit = document.getElementById('Sign In');
-submit.addEventListener("click", function (event) {
-  event.preventDefault()
-  alert(5)
-})
+  // Mostrar los valores en la consola
+  console.log('Nombre:', name);
+  console.log('Apellido:', lastname);
+  console.log('Correo Electrónico:', email);
+  console.log('Contraseña:', password);
+});
